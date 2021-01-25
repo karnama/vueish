3f612938545ec1b0d4cb560eb1f563b038f548f1
fullstackfool <k.a.davies@hotmail.co.uk>
2021-01-25 17:50:24 +0000

    Add vite compilation

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+const path = require('path');
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/main.ts'),
+            name: 'vueish'
+        },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ['vue'],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    vue: 'Vue'
+                }
+            }
+        }
+    },
+    plugins: [vue()]
+});
