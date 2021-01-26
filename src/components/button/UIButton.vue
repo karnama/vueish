a228e362032f0a5121974250a6c851e1a98aa243
fullstackfool <k.a.davies@hotmail.co.uk>
2021-01-26 23:18:49 +0000

    #2 - Remove UIButton test code & tidy

--- src/components/button/UIButton.vue
+++ src/components/button/UIButton.vue
@@ -2,20 +2,8 @@
     <button type="button"
             :class="classes"
             class="vue-ui-button rounded px-4"
-            :disabled="disabled || loading">
-
-        <svg v-if="loading"
-             class="spinner-draw stroke-current"
-             stroke="blue"
-             preserveAspectRatio="xMidYMid"
-             focusable="false"
-             viewBox="0 0 20 20"
-             ref="spinnerDraw">
-            <circle class="spinner-circle stroke-current" cx="50%" cy="50%" r="27"
-                    stroke-width="6 + 'px'" ref="spinnerCircle"></circle>
-        </svg>
-
-        <slot v-else>
+            :disabled="disabled">
+        <slot>
             {{ label }}
         </slot>
     </button>
@@ -23,16 +11,7 @@
 
 <script lang="ts">
 import { computed, defineComponent } from 'vue';
-
-export const TYPES = {
-    default: '',
-    primary: 'text-white bg-primary',
-    info: 'text-white bg-info',
-    success: 'text-white bg-success',
-    warning: 'text-white bg-warning',
-    danger: 'text-white bg-danger',
-    brand: 'text-white bg-brand'
-} as { [key: string]: string; };
+import { types } from './types';
 
 export default defineComponent({
     name: 'UIButton',
@@ -42,11 +21,6 @@ export default defineComponent({
          * String label to use as the button text.
          */
         label: String,
-
-        /**
-         * Boolean flag to toggle loading animation on/off.
-         */
-        loading: Boolean,
 
         /**
          * Boolean flag to enable/disable the button.
@@ -62,22 +36,12 @@ export default defineComponent({
         type: {
             type: String,
             default: 'default',
-            validator: (value: string) => Object.keys(TYPES).includes(value)
+            validator: (value: string) => Object.keys(types).includes(value)
         }
     },
 
-    setup(props) {
-        const classes = computed(() => {
-            if (props.type === 'default') {
-                return props.disabled ? 'bg-dark cursor-not-allowed' : 'hover:bg-dark';
-            }
-
-            if (!props.disabled) {
-                return TYPES[props.type] + ' hover:bg-' + props.type + '-light';
-            }
-
-            return 'bg-' + props.type + '-dark text-white cursor-not-allowed';
-        });
+    setup(props: any) { // TODO-KD: Find better typing
+        const classes = computed(() => types[props.type]);
 
         return { classes };
     }
@@ -88,13 +52,5 @@ export default defineComponent({
 .vue-ui-button {
     min-width: 110px;
     min-height: 35px;
-}
-
-.vue-ui-button:disabled {
-    filter: grayscale(30%);
-}
-
-.vue-ui-button:active:enabled {
-    box-shadow: inset 0 0 100px -3px rgba(0, 0, 0, 0.2);
 }
 </style>
---
+++ src/components/button/types.ts
@@ -0,0 +1,10 @@
+// TODO-KD: Type this better?
+export const types = {
+    default: 'hover:bg-gray-200 active:bg-gray-300',
+    primary: 'text-white bg-blue-600 hover:bg-blue-400 active:bg-blue-700',
+    info: 'text-white bg-blue-400 hover:bg-blue-300 active:bg-blue-500',
+    success: 'text-white bg-green-400 hover:bg-green-300 active:bg-green-500',
+    warning: 'text-white bg-yellow-400 hover:bg-yellow-300 active:bg-yellow-500',
+    danger: 'text-white bg-red-400 hover:bg-red-300 active:bg-red-500',
+    brand: 'text-white bg-brand-400 hover:bg-brand-300 active:bg-brand-500'
+} as { [key: string]: string; };
