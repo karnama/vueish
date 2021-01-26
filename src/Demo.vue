df0b5bd813d8bb93df8bb3844b002645d0d4978f
fullstackfool <k.a.davies@hotmail.co.uk>
2021-01-26 09:30:19 +0000

    #2 - Add demo page

---
+++ src/Demo.vue
@@ -0,0 +1,26 @@
+<template>
+    <main class="h-full container mx-auto bg-gray-50 h-full themed light p-10 text-center">
+        <h1>Vueish Demo</h1>
+        <div class="py-10 space-y-10">
+            <div class="space-x-2">
+                <UIButton>Default</UIButton>
+                <UIButton type="primary">Primary</UIButton>
+                <UIButton type="info">Info</UIButton>
+                <UIButton type="success">Success</UIButton>
+                <UIButton type="warning">Warning</UIButton>
+                <UIButton type="danger">Danger</UIButton>
+                <UIButton type="brand">Brand</UIButton>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import UIButton from './components/button/UIButton.vue';
+
+export default defineComponent({
+    name: 'App',
+    components: { UIButton }
+});
+</script>
---
+++ src/demo.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import Demo from './Demo.vue';
+import './assets/styles/main.css';
+
+createApp(Demo).mount('#app');
