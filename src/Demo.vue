ba579a6cfd9fe2123fa0ff29bbfa26cbcb12cce9
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2021-01-27 17:02:44 +0000

    Tailwind + ts-node = no bueno

--- src/Demo.vue
+++ src/Demo.vue
@@ -1,18 +1,21 @@
 <template>
-    <main class="h-full p-10">
+    <main class="h-full">
         <div class="flex">
-            <div class="mr-10">
-                <h1 class="text-2xl mb-5">Vueish UI</h1>
+            <div class="px-12">
+                <h1 class="text-2xl mt-4 mb-6">
+                    Vueish UI
+                </h1>
                 <div class="space-y-1 flex-col flex">
                     <router-link v-for="route in $router.getRoutes()"
                                  :key="route.path"
-                                 :to="route.path">
+                                 :to="route.path"
+                    >
                         {{ route.meta.label }}
                     </router-link>
                 </div>
             </div>
-            <div class="flex-1 container mx-auto bg-gray-50 h-full themed light p-10 text-center">
-                <router-view/>
+            <div class="flex-1 h-screen container mx-auto bg-gray-50 h-full themed light p-10 text-center">
+                <router-view />
             </div>
         </div>
     </main>
