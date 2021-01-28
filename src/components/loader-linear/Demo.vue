4b4bde77187513e638d39bcb68af14ffbdb7fa0a
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2021-01-28 12:45:34 +0000

    #10 - Added Linear Loader component

---
+++ src/components/loader-linear/Demo.vue
@@ -0,0 +1,13 @@
+<template>
+    <UILinearLoader />
+</template>
+
+<script lang="ts">
+import UILinearLoader from './UILinearLoader.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'LinearLoader',
+    components: { UILinearLoader }
+});
+</script>
