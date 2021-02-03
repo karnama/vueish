8a05ebc516947ea9cd84e776c1249f8ef4a20713
fullstackfool <k.a.davies@hotmail.co.uk>
2021-02-03 22:31:00 +0000

    #25 - wip

---
+++ src/components/icon/Demo.vue
@@ -0,0 +1,17 @@
+<template>
+    <UIIcon plus-circle />
+    <UIIcon x-circle />
+    <UIIcon database />
+    <UIIcon credit-card />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import UIIcon from '@components/icon/UIIcon.vue';
+
+export default defineComponent({
+    name: 'Icon',
+
+    components: { UIIcon }
+});
+</script>
