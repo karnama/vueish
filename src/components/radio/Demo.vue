bd9b1313bb870180557fca094df77c543ccc0567
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2021-02-11 16:10:48 +0000

    #55 - Fixed Radio component

--- src/components/radio/Demo.vue
+++ src/components/radio/Demo.vue
@@ -5,8 +5,10 @@
 
     <UIRadioGroup v-model="value"
                   name="gender"
-                  required
-                  label="Vertical/Required">
+                  class="mb-10">
+        <template #label>
+            Vertical
+        </template>
         <UIRadio value="male"
                  label="Male" />
         <UIRadio value="female"
@@ -15,54 +17,20 @@
                  label="Other" />
     </UIRadioGroup>
 
-    <div class="flex items-center">
-        <UIRadioGroup v-model="value"
-                      name="gender"
-                      required
-                      label="Vertical/Required">
-            <UIRadio value="male"
-                     label="Male" />
-            <UIRadio value="female"
-                     label="Female" />
-            <UIRadio value="other"
-                     label="Other" />
-        </UIRadioGroup>
-
-        <div class="flex-col space-y-6">
-            <UIRadioGroup v-model="value2"
-                          name="gender2"
-                          class="ml-10"
-                          horizontal>
-                <template #label>
-                    Horizontal
-                </template>
-                <UIRadio value="male"
-                         label="Male" />
-                <UIRadio value="female"
-                         label="Female" />
-                <UIRadio value="other"
-                         label="Other" />
-            </UIRadioGroup>
-
-            <UIRadioGroup v-model="value2"
-                          name="gender3"
-                          class="ml-10"
-                          horizontal
-                          disabled>
-                <template #label>
-                    Horizontal/Disabled
-                </template>
-                <UIRadio value="male"
-                         label="Male"
-                         :disabled="false" />
-                <UIRadio value="female"
-                         label="Female" />
-                <UIRadio value="other">
-                    Other
-                </UIRadio>
-            </UIRadioGroup>
-        </div>
-    </div>
+    <UIRadioGroup v-model="value2"
+                  name="gender2"
+                  horizontal
+                  label="Horizontal/Disabled"
+                  disabled>
+        <UIRadio value="male"
+                 label="Male"
+                 :disabled="false" />
+        <UIRadio value="female"
+                 label="Female" />
+        <UIRadio value="other">
+            Other
+        </UIRadio>
+    </UIRadioGroup>
 </template>
 
 <script lang="ts">
@@ -76,7 +44,7 @@ export default defineComponent({
     components: { UIRadioGroup, UIRadio },
 
     setup() {
-        const value = ref(null);
+        const value = ref();
         const value2 = ref('male');
 
         return { value, value2 };
