80eea1ebb0e3d19621e601feeeb9eeaad936736a
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2021-03-05 09:53:06 +0000

    #85 - Added button group

---
+++ src/components/button-group/Demo.vue
@@ -0,0 +1,24 @@
+<template>
+    <UIButtonGroup>
+        <UIButton type="primary">
+            Places
+        </UIButton>
+        <UIButton type="primary">
+            List
+        </UIButton>
+        <UIButton type="primary">
+            Map
+        </UIButton>
+    </UIButtonGroup>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import UIButtonGroup from '@components/button-group/UIButtonGroup.vue';
+import UIButton from '@components/button/UIButton.vue';
+
+export default defineComponent({
+    name: 'ButtonGroup',
+    components: { UIButton, UIButtonGroup }
+});
+</script>
---
+++ src/components/button-group/UIButtonGroup.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="button-group" :class="{ vertical, spread }">
+        <slot />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'UIButtonGroup',
+
+    props: {
+        vertical: {
+            type: Boolean,
+            default: false
+        },
+        spread: {
+            type: Boolean,
+            default: false
+        }
+    }
+});
+</script>
+
+<style lang="scss">
+.button-group {
+    @apply flex items-center;
+    & > button {
+        @apply rounded-none overflow-hidden;
+    }
+    & > button:last-child {
+        @apply rounded-r;
+    }
+    & > button:first-child {
+        @apply rounded-l;
+    }
+}
+.vertical {
+    @apply flex-col items-start;
+    & > button:last-child {
+        @apply rounded-b rounded-r-none;
+    }
+    & > button:first-child {
+        @apply rounded-t rounded-l-none;
+    }
+}
+.spread {
+    & > button {
+        @apply flex-1 w-full;
+    }
+}
+</style>
