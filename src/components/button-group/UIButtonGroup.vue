9359795007c66b8d5d09b904bfce69ef17b0cb36
nandi95 <41805560+nandi95@users.noreply.github.com>
2021-03-29 12:14:41 +0100

    #85 - Added button group feature (#87)
    * #85 - updated UIText
    
    * #85 - renamed to UIInput
    
    * #85 - updated UITextarea
    
    * #85 - Added button group
    
    * #85 Added button toggle group
    
    * #85 - Added large style
    
    * #85 - Added button styles
    
    * #85 - Remove flex from UIButton demo
    
    No needed at it was messing with the button
    styles in some cases.
    
    * #85 - Adjust UIInput demo
    
    * #85 - added button active class
    
    * #85 - Update deps
    
    * #85 - Tweak button styles
    
    Co-authored-by: fullstackfool <k.a.davies@hotmail.co.uk>

---
+++ src/components/button-group/UIButtonGroup.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="button-group" :class="{ spread, vertical }" role="group">
+        <slot />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export const props = {
+    /**
+     * Display the buttons in a vertical order.
+     */
+    vertical: {
+        type: Boolean,
+        default: false
+    },
+
+    /**
+     * Set the width evenly to screen size.
+     */
+    spread: {
+        type: Boolean,
+        default: false
+    }
+};
+
+export default defineComponent({
+    name: 'UIButtonGroup',
+
+    props
+});
+</script>
+
+<style lang="scss">
+.button-group {
+    @apply flex items-center;
+    & > button {
+        @apply rounded-none overflow-hidden;
+    }
+    & > button:last-child {
+        @apply rounded-r;
+    }
+    & > button:first-child {
+        @apply rounded-l;
+    }
+}
+.vertical {
+    @apply flex-col items-start w-max;
+    & > button {
+        @apply flex-1 w-full;
+    }
+    & > button:last-child {
+        @apply rounded-b rounded-r-none;
+    }
+    & > button:first-child {
+        @apply rounded-t rounded-l-none;
+    }
+}
+.spread {
+    @apply w-full;
+    & > button {
+        @apply flex-1 w-full;
+    }
+}
+</style>
