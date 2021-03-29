0ec8d118c92fdec94a8f8a8f6f351900dad82625
nandi95 <41805560+nandi95@users.noreply.github.com>
2021-03-29 13:32:23 +0100

    #106 - UIAvatar: Add component (#109)

--- src/types/public/components/index.ts
+++ src/types/public/components/index.ts
@@ -11,3 +11,31 @@ export type Option = {
     slot?: string; // name of the slot to use for this option
     category?: StyleType;
 };
+
+// UIAvatar
+export interface Avatar {
+    src?: string;
+    alt?: string;
+    content?: string;
+    squared?: boolean;
+}
+
+// UITable
+export interface Row {
+    isSelectable?: boolean; // whether the row is selectable
+    [key: string]: any;
+}
+export interface Column<T = Row> {
+    header?: string;
+    rowProperty: string;
+    suffix?: string;
+    prefix?: string;
+    sortable?: boolean;
+    sortByFunc?: SortByFunc<T>;
+}
+export interface SortOrder {
+    column: string;
+    sortByFunc?: SortByFunc<Row>;
+    direction: 'asc' | 'desc';
+}
+type SortByFunc<T> = (row: T) => any;
