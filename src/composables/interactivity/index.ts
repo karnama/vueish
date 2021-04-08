7a9ba930d8f71960a6a85926ffb22e4f4f534d98
nandi95 <41805560+nandi95@users.noreply.github.com>
2021-04-08 10:45:38 +0100

    #95 - Added context menu (#99)

---
+++ src/composables/interactivity/index.ts
@@ -0,0 +1,1 @@
+export { default as clickAway } from './clickAway';
--- src/composables/style/index.ts
+++ src/composables/style/index.ts
@@ -24,3 +24,30 @@ export const large: Prop<boolean, false> = {
     type: Boolean,
     default: false
 };
+
+/**
+ * Convert numbers and vh/vw values to px values.
+ */
+export const getPxValue = (value: string | number): number => {
+    if (typeof value === 'number') {
+        return value;
+    }
+
+    if (!isNaN(Number(value))) {
+        return Number(value);
+    }
+
+    if (value.endsWith('px')) {
+        return Number(value.slice(0, -2));
+    }
+
+    if (value.endsWith('vw')) {
+        return document.documentElement.clientWidth / 100 * Number(value.slice(0, -2));
+    }
+
+    if (value.endsWith('vh')) {
+        return document.documentElement.clientHeight / 100 * Number(value.slice(0, -2));
+    }
+
+    throw new TypeError('Unexpected argument given.');
+};
