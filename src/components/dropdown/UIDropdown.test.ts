a9a53ba7a1b70cd01d7748fc9baf1fef56a3a80c
fullstackfool <k.a.davies@hotmail.co.uk>
2021-04-08 10:40:36 +0100

    #95 - Fix tests

--- src/components/dropdown/UIDropdown.test.ts
+++ src/components/dropdown/UIDropdown.test.ts
@@ -6,17 +6,17 @@ describe('UIDropdown', () => {
     it('should display the given content when open', async () => {
         const wrapper = mount(UIDropdown, {
             slots: {
-                default: h('div', { id: 'default-content' }),
-                menu: h('div', { id: 'content' })
+                default: h('div', { id: 'content' }),
+                trigger: h('div', { id: 'trigger' })
             }
         });
 
-        expect(wrapper.find('#default-content').exists()).toBe(true);
+        expect(wrapper.find('#trigger').exists()).toBe(true);
         expect(wrapper.find('#content').exists()).toBe(false);
         // @ts-expect-error
         wrapper.vm.toggle();
         await nextTick();
         expect(wrapper.find('#content').exists()).toBe(true);
-        expect(wrapper.find('#default-content').exists()).toBe(true);
+        expect(wrapper.find('#trigger').exists()).toBe(true);
     });
 });
