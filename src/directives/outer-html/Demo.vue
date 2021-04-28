4ff6b6bf5fe12067516efadd197ea7f3d811c2e5
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2021-04-28 12:22:13 +0100

    #78 - Removed binding update

--- src/directives/outer-html/Demo.vue
+++ src/directives/outer-html/Demo.vue
@@ -1,36 +1,34 @@
 <template>
-<!--    <div v-outer-html="html">-->
-<!--        Origin div-->
-<!--    </div>-->
-<!--    <span>This is after the inserted html</span>-->
+    <div v-outer-html="html">
+        Origin div
+    </div>
+    <span>This is after the inserted html</span>
 
-<!--    <hr class="my-4">-->
+    <hr class="my-4">
 
     <template v-outer-html="templateHtml" />
 
-    <UIButton label="Change html" @click="change" />
+    <br>
+
+    <small class="bg-red-600 shadow rounded text-white p-2">
+        Dynamic values are not viable as it is uncertain hom many nodes has been created before
+    </small>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import UIButton from '@components/button/UIButton.vue';
+import outerHtml from './index';
 
 export default defineComponent({
     name: 'OuterHtml',
-    components: { UIButton },
+    directives: { outerHtml },
     setup() {
         const html = ref('<p>This is the next sibling of the origin</p>');
         const templateHtml = ref('<p>This has replaced the &lt;template&gt; tag</p>');
 
-        const change = () => {
-            html.value += '<p>updated</p>';
-            templateHtml.value = templateHtml.value + '<p>updated</p>';
-        };
-
         return {
             html,
-            templateHtml,
-            change
+            templateHtml
         };
     }
 });
--- src/directives/outer-html/index.ts
+++ src/directives/outer-html/index.ts
@@ -3,10 +3,5 @@ import type { Directive, DirectiveBinding } from 'vue';
 export default {
     mounted: (el: Element, binding: DirectiveBinding<string>) => {
         el.insertAdjacentHTML('afterend', binding.value);
-    },
-    updated: (el: Element, binding: DirectiveBinding<string>) => {
-        // console.log(el.nextElementSibling!.outerHTML);
-        // console.log(binding.value);
-        el.nextElementSibling!.outerHTML = binding.value;
     }
 } as Directive;
