efb9c319e9bdcf1bc5667050b5ab88d023eb960b
nandi95 <41805560+nandi95@users.noreply.github.com>
2021-05-18 09:50:17 +0100

    #78 - Add directive: outer html (#110)

---
+++ src/directives/outer-html/Demo.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-outer-html="html">
+        Origin div
+    </div>
+    <span>This is after the inserted html</span>
+
+    <hr class="my-4">
+
+    <template v-outer-html="templateHtml" />
+
+    <br>
+
+    <small class="bg-red-600 shadow rounded text-white p-2">
+        Dynamic values are not viable as it is uncertain how many nodes has been created before
+    </small>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import outerHtml from './index';
+
+export default defineComponent({
+    name: 'OuterHtml',
+    directives: { outerHtml },
+    setup() {
+        const html = ref('<p>This is the next sibling of the origin</p>');
+        const templateHtml = ref('<p>This has replaced the &lt;template&gt; tag</p>');
+
+        return {
+            html,
+            templateHtml
+        };
+    }
+});
+</script>
