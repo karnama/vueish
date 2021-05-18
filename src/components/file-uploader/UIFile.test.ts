d0020d75d1a951de59cb9c6f08b8f12e291c5a69
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2021-05-18 15:38:33 +0100

    #150 - Added UIFile test

--- src/components/file-uploader/UIFile.test.ts
+++ src/components/file-uploader/UIFile.test.ts
@@ -1,11 +1,12 @@
 import { mount } from '@vue/test-utils';
 import UIFile from '@components/file-uploader/UIFile.vue';
+import { nextTick } from 'vue';
 
 describe('UIFile', () => {
     const file = new File(['foo'], 'foo.txt', {
         type: 'text/plain'
     });
-    const upload = jest.fn();
+    const upload = () => {};
 
     it('should display correctly', () => {
         const wrapper = mount(UIFile, {
@@ -18,9 +19,75 @@ describe('UIFile', () => {
         expect(wrapper.element).toMatchSnapshot();
     });
 
-    it.todo('should display the size in the kb or mb');
+    it('should display the size in the kb or mb', async () => {
+        const wrapper = mount(UIFile, {
+            props: {
+                file,
+                upload
+            }
+        });
 
-    it.todo('should emit an event with the removed file on remove');
+        expect(wrapper.html()).toContain('0.0029 KB');
+        const newFile = new File([Array(100000).fill('foo').join()], 'foo.txt', {
+            type: 'text/plain'
+        });
 
-    it.todo('should display an image if the file is an image');
+        await wrapper.setProps({ file: newFile });
+        expect(wrapper.html()).toContain('0.38 MB');
+    });
+
+    it('should emit an event with the removed file on remove', async () => {
+        const wrapper = mount(UIFile, {
+            props: {
+                file,
+                upload
+            }
+        });
+
+        await wrapper.get('button').trigger('click');
+
+        expect(wrapper.lastEventValue<[File]>('removed')![0].size).toStrictEqual(file.size);
+    });
+
+    // tried mocking the reader.result too to no avail
+    // eslint-disable-next-line jest/no-disabled-tests
+    it.skip('should display an image if the file is an image', async () => {
+        const base64 = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3a' +
+            'AAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII=';
+        const imgFile = new File(['data:image/png;base64,' + base64], '1x1.png', {
+            type: 'image/png'
+        });
+        const wrapper = mount(UIFile, {
+            props: {
+                file: imgFile,
+                upload
+            }
+        });
+
+        expect(wrapper.find('img').isVisible()).toBe(true);
+        await wrapper.setProps({ file });
+        expect(wrapper.find('img').isVisible()).toBe(false);
+    });
+
+    it('should call the given upload if uploading on mounted', () => {
+        const upload = jest.fn();
+        mount(UIFile, {
+            props: {
+                file,
+                upload
+            }
+        });
+
+        expect(upload).not.toHaveBeenCalled();
+
+        mount(UIFile, {
+            props: {
+                file,
+                upload,
+                uploadOnMounted: true
+            }
+        });
+
+        expect(upload).toHaveBeenCalledWith(file);
+    });
 });
