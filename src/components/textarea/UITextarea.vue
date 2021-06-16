3a52d026d802daac3fbc98b3f7247e8ab9181c7f
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2021-06-16 17:12:02 +0100

    #210 - Added textarea dark styles

--- src/components/textarea/UITextarea.vue
+++ src/components/textarea/UITextarea.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="ui-textarea relative">
-        <label :for="$attrs.id ?? name" class="font-medium text-gray-700">
+        <label :for="$attrs.id ?? name" class="font-medium text-color">
             {{ label }}
         </label>
 
-        <div class="group relative shadow border border-gray-300 rounded bg-white
-                    transition focus-within:border-blue-400"
-             :class="{ 'bg-gray-100': disabled }">
+        <div class="group relative shadow border border-gray-300 rounded bg-white dark:bg-gray-600 dark:shadow-md
+                    transition focus-within:border-blue-400 dark:focus-within:border-blue-500"
+             :class="{ 'bg-gray-200 dark:!bg-gray-700': disabled }">
             <div class="flex items-center items-stretch">
                 <textarea :id="$attrs.id ?? name"
                           v-bind="$attrs"
@@ -16,18 +16,18 @@
                           :disabled="disabled"
                           :aria-placeholder="$attrs.placeholder"
                           class="flex-1 p-3 appearance-none bg-transparent outline-none
-                                 disabled:cursor-not-allowed disabled:text-gray-400"
+                                 disabled:cursor-not-allowed text-color disabled:text-gray-400"
                           :style="[
                               disabled || fixed || autoSize ? 'resize: none' : '',
                               counter ? 'min-height: 5rem' : '']" />
 
                 <div class="flex flex-col justify-center" :class="{ 'text-gray-400 cursor-not-allowed': disabled }">
                     <span v-if="disabled"
-                          class="h-5 w-5 mx-2 text-gray-400 flex-grow align-middle flex flex-col justify-center"
+                          class="h-5 w-5 mx-2 text-color-muted flex-grow align-middle flex flex-col justify-center"
                           v-html="lockIcon" />
 
                     <button v-else-if="clearable && model"
-                            class="clear-icon h-5 w-5 mx-2 text-gray-500"
+                            class="clear-icon h-5 w-5 mx-2 text-color-muted"
                             :aria-controls="$attrs.id ?? name"
                             aria-roledescription="clear"
                             @click="model = ''"
@@ -36,7 +36,7 @@
             </div>
         </div>
         <span v-if="counter && modelValue"
-              class="text-sm px-1 text-center h-5 -bottom-5 right-0 absolute text-gray-500">
+              class="text-sm px-1 text-center h-5 -bottom-5 right-0 absolute text-color-muted">
             {{ modelValue.length }}
         </span>
     </div>
