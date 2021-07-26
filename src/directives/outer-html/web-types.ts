053bceb2b123a2d79c204855ade39dd46b9eb9d7
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2021-07-26 17:27:40 +0100

    feat(outer-html): added web-types file

---
+++ src/directives/outer-html/web-types.ts
@@ -0,0 +1,6 @@
+import type { HtmlAttribute } from '../../../utils/web-types';
+
+export default {
+    description: 'Insert the html as the next sibling element. This can be used on a `<template>` tag.' +
+        'However this isn\'t reactive'
+} as HtmlAttribute;
