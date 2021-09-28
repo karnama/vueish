943d4c9524421df143971633a1e3fa9b08d04d9f
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2021-09-28 08:02:07 +0100

    test: trigger tests

--- src/components/avatar/UIAvatar.test.ts
+++ src/components/avatar/UIAvatar.test.ts
@@ -20,7 +20,7 @@ describe('UIAvatar', () => {
     it('should display alt\'s first letter if given and source cannot load and slot not given', () => {
         const wrapper = mount(UIAvatar, {
             props: {
-                alt: 'my alt text'
+                alt: 'my alt textd'
             }
         });
 
