f185f723eabc8ae193b885486cb77f3aa4451bad
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2022-03-25 18:17:44 +0000

    fix(intersect): fixed callback being run twice

--- src/directives/intersect/index.ts
+++ src/directives/intersect/index.ts
@@ -1,5 +1,5 @@
 import type { Directive, DirectiveBinding, DirectiveHook } from 'vue';
-import type { IntersectArgument, IntersectCallback } from 'types';
+import type { IntersectArgument, IntersectCallback, IntersectObject } from 'types';
 
 interface ElementToObserve extends Element {
     _intersectionObserver?: {
@@ -8,6 +8,9 @@ interface ElementToObserve extends Element {
     };
 }
 
+type Modifiers = Omit<IntersectObject, 'callback' | 'root' | 'rootMargin' | 'threshold'>;
+
+// todo - modifiers for enter,leave
 const setUpObserver: DirectiveHook = (
     el: ElementToObserve,
     binding: DirectiveBinding<IntersectArgument>
@@ -22,20 +25,28 @@ const setUpObserver: DirectiveHook = (
             initiated: false,
             observer: new IntersectionObserver((entries) => {
                 const entry = entries.find(e => e.isIntersecting);
+                const modifiers = binding.modifiers as Modifiers;
 
-                binding.modifiers.idle
+                const cb = modifiers.idle
                     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-                    ? requestIdleCallback(() => (binding.value as IntersectCallback)(!!entry, entry))
-                    : (binding.value as IntersectCallback)(!!entry, entry);
+                    ? () => requestIdleCallback(() => (binding.value as IntersectCallback)(!!entry, entry))
+                    : () => (binding.value as IntersectCallback)(!!entry, entry);
+
                 // callback runs on setup so push to the next event cycle
                 initiate(el);
 
-                if (binding.modifiers.once && el._intersectionObserver?.initiated) {
-                    unbindObserver(el);
+                if (el._intersectionObserver?.initiated) {
+                    cb();
+
+                    if (binding.modifiers.once) {
+                        unbindObserver(el);
+                    }
                 }
             })
         };
     } else {
+        const enter = binding.value.hasOwnProperty('enter') ? !!binding.value.enter : binding.modifiers.enter;
+        const leave = binding.value.hasOwnProperty('leave') ? !!binding.value.leave : binding.modifiers.leave;
         const idle = binding.value.hasOwnProperty('idle') ? !!binding.value.idle : binding.modifiers.idle;
         const once = binding.value.hasOwnProperty('once') ? !!binding.value.once : binding.modifiers.once;
         const { callback, threshold, root, rootMargin } = binding.value;
@@ -49,15 +60,20 @@ const setUpObserver: DirectiveHook = (
             observer: new IntersectionObserver((entries) => {
                 const entry = entries.find(e => e.isIntersecting);
 
-                idle
+                const cb = idle
                     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-                    ? requestIdleCallback(() => callback(!!entry, entry))
-                    : callback(!!entry, entry);
+                    ? () => requestIdleCallback(() => void callback(!!entry, entry))
+                    : () => void callback(!!entry, entry);
+
                 // callback runs on setup so push to the next event cycle
                 initiate(el);
 
-                if (once && el._intersectionObserver && el._intersectionObserver?.initiated) {
-                    unbindObserver(el);
+                if (el._intersectionObserver?.initiated) {
+                    cb();
+
+                    if (binding.modifiers.once) {
+                        unbindObserver(el);
+                    }
                 }
             }, {
                 threshold,
